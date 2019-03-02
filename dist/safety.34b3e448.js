46f2b35f54da91a04725e2674a1b132b95b44913
yzs311 <736645279@qq.com>
2019-03-02 18:29:30 +0800

    完成安全管理页面车辆出入模块

--- dist/safety.34b3e448.js
+++ dist/safety.34b3e448.js
@@ -115,7 +115,9 @@ $(function () {
     }
   }); // 初始化设备信息滚动
 
-  var swiper2 = new Swiper('#swiper2'); // const swiper3 = new Swiper('#swiper3')
+  var swiper2 = new Swiper('#swiper2', {
+    spaceBetween: 30
+  }); // const swiper3 = new Swiper('#swiper3')
   // 模块切换点击事件
   // 切换到塔吊模块
 
@@ -136,8 +138,59 @@ $(function () {
       $(siblings[i]).css('display', 'none');
     }
 
-    var swiper3 = new Swiper('#swiper3');
-  });
+    var swiper3 = new Swiper('#swiper3', {
+      spaceBetween: 30
+    });
+  }); // 切换到车辆出入模块
+
+  $('#carBtn').on('click', function () {
+    $('.car').css('display', '');
+    var siblings = $('.car').siblings();
+
+    for (var i = 0; i < siblings.length; i++) {
+      $(siblings[i]).css('display', 'none');
+    }
+
+    var swiper4 = new Swiper('#swiper4', {
+      spaceBetween: 30
+    });
+    scrollStart('carBox1', 'carBox1-1', 'carBox1-2');
+    scrollStart('carBox2', 'carBox2-1', 'carBox2-2');
+  }); // 滚动启动函数
+
+  function scrollStart(id, id1, id2) {
+    setTimeout(function () {
+      var speed = 45;
+      var colee2 = document.getElementById(id2);
+      var colee1 = document.getElementById(id1);
+      var colee = document.getElementById(id);
+      colee2.innerHTML = colee1.innerHTML; //克隆colee1为colee2
+
+      function Marquee1() {
+        // console.log(colee1.offsetHeight)
+        //当滚动至colee1与colee2交界时
+        if (colee2.offsetTop - colee.scrollTop <= 0) {
+          colee.scrollTop -= colee1.offsetHeight; //colee跳到最顶端
+        } else {
+          colee.scrollTop++;
+
+          if (colee.scrollTop == colee1.offsetHeight) {
+            colee.scrollTop = 0;
+          }
+        }
+      }
+
+      var MyMar1 = setInterval(Marquee1, 50); //设置定时器
+      //   //鼠标移上时清除定时器达到滚动停止的目的
+      //   colee.onmouseover = function() {
+      //     clearInterval(MyMar1);
+      //   };
+      //   //鼠标移开时重设定时器
+      //   colee.onmouseout = function() {
+      //     MyMar1 = setInterval(Marquee1, speed);
+      //   };
+    }, 100);
+  }
 });
 },{}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -166,7 +219,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51612" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49511" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- js/safety.js
+++ js/safety.js
@@ -8,7 +8,9 @@ $(function(){
         },
     })
     // 初始化设备信息滚动
-    const swiper2 = new Swiper('#swiper2')
+    const swiper2 = new Swiper('#swiper2',{
+        spaceBetween : 30,
+    })
     // const swiper3 = new Swiper('#swiper3')
     // 模块切换点击事件
     // 切换到塔吊模块
@@ -26,6 +28,52 @@ $(function(){
         for (let i = 0; i < siblings.length; i++) {
             $(siblings[i]).css('display','none')
         }
-        const swiper3 = new Swiper('#swiper3')
+        const swiper3 = new Swiper('#swiper3',{
+            spaceBetween : 30,
+        })
     })
+    // 切换到车辆出入模块
+    $('#carBtn').on('click',()=>{
+        $('.car').css('display','')
+        let siblings = $('.car').siblings()
+        for (let i =0; i < siblings.length; i++) {
+            $(siblings[i]).css('display','none')
+        }
+        const swiper4 = new Swiper('#swiper4',{
+            spaceBetween : 30,
+        })
+        scrollStart('carBox1','carBox1-1','carBox1-2')
+        scrollStart('carBox2','carBox2-1','carBox2-2')
+    })
+    // 滚动启动函数
+    function scrollStart(id,id1,id2) {
+        setTimeout(() => {
+          var speed = 45;
+          var colee2 = document.getElementById(id2);
+          var colee1 = document.getElementById(id1);
+          var colee = document.getElementById(id);
+          colee2.innerHTML = colee1.innerHTML; //克隆colee1为colee2
+          function Marquee1() {
+            // console.log(colee1.offsetHeight)
+            //当滚动至colee1与colee2交界时
+            if (colee2.offsetTop - colee.scrollTop <= 0) {
+              colee.scrollTop -= colee1.offsetHeight; //colee跳到最顶端
+            } else {
+              colee.scrollTop++;
+              if (colee.scrollTop == colee1.offsetHeight) {
+                colee.scrollTop = 0;
+              }
+            }
+          }
+          var MyMar1 = setInterval(Marquee1,50); //设置定时器
+        //   //鼠标移上时清除定时器达到滚动停止的目的
+        //   colee.onmouseover = function() {
+        //     clearInterval(MyMar1);
+        //   };
+        //   //鼠标移开时重设定时器
+        //   colee.onmouseout = function() {
+        //     MyMar1 = setInterval(Marquee1, speed);
+        //   };
+        }, 100);
+    }
 })
