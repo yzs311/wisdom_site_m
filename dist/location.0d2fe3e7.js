3b4e72618bc1fce31caf0daea2621b92425f4d5a
yzs311 <736645279@qq.com>
2019-03-11 18:17:37 +0800

    修改样式

--- dist/location.0d2fe3e7.js
+++ dist/location.0d2fe3e7.js
@@ -122,12 +122,12 @@ $(function () {
         left: '0'
       });
       $('.search').animate({
-        left: '108%'
+        left: '135%'
       });
       side = 1;
     } else {
       $('.side-box').animate({
-        left: '-2.15rem'
+        left: '-3.2rem'
       });
       $('.search').animate({
         left: '50%'
@@ -142,20 +142,171 @@ $(function () {
     } else {
       $('.search-data').css('display', 'none');
     }
-  }); // 查看人员信息点击事件
+  });
+  var marker;
+  var polygon; // 查看人员信息点击事件
 
   $('#examine').on('click', function () {
     $('.data-box').css('display', 'block');
     $('.search-data').css('display', 'none');
-  }); // 人员信息与历史轨迹切换
+    marker = new AMap.Marker({
+      position: [114.003378, 22.571492] // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+
+    });
+    map.add(marker);
+    polygon = new AMap.Polygon({
+      path: [new AMap.LngLat(113.992992, 22.581439), new AMap.LngLat(114.01033, 22.582786), new AMap.LngLat(114.010287, 22.562853), new AMap.LngLat(113.996898, 22.563329)],
+      fillColor: '#fff',
+      // 多边形填充颜色
+      fillOpacity: 0,
+      //填充颜色透明度
+      borderWeight: 1,
+      // 线条宽度
+      strokeColor: '#137ed2' // 线条颜色}
+
+    });
+    map.add(polygon);
+    map.setZoomAndCenter(14, [114.003378, 22.561492]);
+  });
+  var circle1;
+  var circle2;
+  var circle3;
+  var circle4;
+  var polyline; // 人员信息与历史轨迹切换
 
   $('#particular').on('click', function () {
     $('.data-box').css('display', 'block');
     $('.history-box').css('display', 'none');
+    map.add(marker);
+    map.remove(circle1);
+    map.remove(circle2);
+    map.remove(circle3);
+    map.remove(circle4);
+    map.remove(polyline);
   });
   $('#history').on('click', function () {
     $('.data-box').css('display', 'none');
     $('.history-box').css('display', 'block');
+    circle1 = new AMap.Circle({
+      center: [113.994194, 22.578189],
+      fillOpacity: 1,
+      //透明度
+      zIndex: 100,
+      //层级
+      radius: 10,
+      //半径
+      fillColor: '#e10505',
+      //填充颜色
+      strokeColor: '#e10505' //轮廓线颜色
+
+    });
+    circle2 = new AMap.Circle({
+      center: [114.004537, 22.563725],
+      fillOpacity: 1,
+      //透明度
+      zIndex: 100,
+      //层级
+      radius: 10,
+      //半径
+      fillColor: '#e10505',
+      //填充颜色
+      strokeColor: '#e10505' //轮廓线颜色
+
+    });
+    circle3 = new AMap.Circle({
+      center: [114.009601, 22.570383],
+      fillOpacity: 1,
+      //透明度
+      zIndex: 100,
+      //层级
+      radius: 10,
+      //半径
+      fillColor: '#e10505',
+      //填充颜色
+      strokeColor: '#e10505' //轮廓线颜色
+
+    });
+    circle4 = new AMap.Circle({
+      center: [114.006167, 22.580171],
+      fillOpacity: 1,
+      //透明度
+      zIndex: 100,
+      //层级
+      radius: 10,
+      //半径
+      fillColor: '#e10505',
+      //填充颜色
+      strokeColor: '#e10505' //轮廓线颜色
+
+    });
+    polyline = new AMap.Polyline({
+      path: [new AMap.LngLat(113.994194, 22.578189), new AMap.LngLat(114.004537, 22.563725), new AMap.LngLat(114.009601, 22.570383), new AMap.LngLat(114.006167, 22.580171)],
+      lineJoin: 'round',
+      //折线拐点样式
+      showDir: true,
+      //移动方向
+      strokeWeight: 3,
+      //线条宽度
+      strokeColor: '#3366ff' //线条颜色
+
+    });
+    map.add(circle1);
+    map.add(circle2);
+    map.add(circle3);
+    map.add(circle4);
+    map.add(polyline);
+    map.remove(marker);
+  });
+  var temp = 1; // 历史轨迹详细信息
+
+  $('#historyTop').on('click', function () {
+    if (temp == 1) {
+      $('#historyMiddle').animate({
+        height: '1.705rem'
+      });
+      temp = 0;
+    } else {
+      $('#historyMiddle').animate({
+        height: '0'
+      });
+      temp = 1;
+    }
+  });
+  var project = 1; // 一级菜单
+
+  $('#project').on('click', function () {
+    if (project == 1) {
+      $('#project').animate({
+        height: '3.19rem'
+      });
+      project = 0;
+      $('.blue-v').addClass('rotate');
+    } else {
+      $('#project').animate({
+        height: '.39rem'
+      });
+      $('.blue-v').removeClass('rotate');
+      project = 1;
+    }
+  });
+  var subProject = 1; // 二级菜单
+
+  $('#subProject').on('click', function (event) {
+    event.stopPropagation();
+
+    if (subProject == 1) {
+      $('#subProject').animate({
+        height: '1.4rem'
+      });
+      $('.black-v').addClass('rotate');
+      subProject = 0;
+    } else {
+      $('#subProject').animate({
+        height: '.35rem'
+      });
+      $('.black-v').removeClass('rotate');
+      subProject = 1;
+    }
   });
 });
 },{}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -185,7 +336,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61488" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49515" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
